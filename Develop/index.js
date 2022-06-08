8abaae7e0f1f6b7261583e9610e36be31955774f
astokes <za.stokes@gmail.com>
2022-06-08 13:40:10 -0400

    first commit

---
+++ Develop/index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const gm = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  generateMarkdown(data);
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
